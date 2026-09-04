de053189d2c580a0139ced9473b715f4fd210873
Dhruv-Rathor <dhruvrathor741@gmail.com>
2026-09-05 01:15:35 +0530

    Update MainActivity.java

--- app/src/main/java/com/dhruv/glaautologin/MainActivity.java
+++ app/src/main/java/com/dhruv/glaautologin/MainActivity.java
@@ -1,18 +1,30 @@
 package com.dhruv.glaautologin;
+
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
+
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Request Notification Permission for Android 13+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 101);
+            }
+        }
+
         Intent serviceIntent = new Intent(this, AutoLoginService.class);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             startForegroundService(serviceIntent);
         } else {
             startService(serviceIntent);
         }
+        
         finish();
     }
 }
